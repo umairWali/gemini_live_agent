4d92e9f732073c6efddd1a54c0dcb4c665e639ee
Musab Khan <khan2614004@gmail.com>
2026-03-06 02:13:20 +0100

    Cleanup useless debug files

--- sidecar/tts.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
-execSync('espeak -w test.wav "Hello, how are you doing today? I am fine."');
-execSync('ffmpeg -i test.wav -acodec pcm_s16le -ar 24000 -ac 1 test.raw -y');
